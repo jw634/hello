76805cc668f16e93ba96a2dbc00f741d6a3663a2
Joshua Wichman <jwichman2@huskers.unl.edu>
2022-08-25 18:27:00 +0000

    intial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Joshua Wichman
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
